fc17bb68e9e7dfbf51bcd9c664ce8b27f665a07d
guozhiyu <843505499@qq.com>
2020-03-14 15:13:39 +0800

    第一次提交

--- allurelove/src/main/java/com/itheima/demo/SaySomething.java
+++ allurelove/src/main/java/com/itheima/demo/SaySomething.java
@@ -2,6 +2,9 @@ package com.itheima.demo;
 
 public class SaySomething {
     public static void main(String[] args) {
-
+        System.out.println("好无聊啊!");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("流水落花春去也,天上人间");
+        }
     }
 }
